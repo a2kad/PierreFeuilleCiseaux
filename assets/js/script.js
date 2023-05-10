21960ca3f9edf1da1b305d129d1f285866193e48
a2kad <reshetnikovruslan705@gmail.com>
2023-05-10 15:02:22 +0200

    Finished button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 let joueur = document.getElementById('joueur')
 let ordinateur = document.getElementById('ordinateur')
-let verify = document.getElementById('verify')
 let play = document.getElementById('play')
+let reset = document.getElementById('reset')
 let radioButtons = document.querySelectorAll('input[name="btnradio"]')
 
 let myGame = ''
@@ -9,36 +9,37 @@ let computerGame = ''
 
 radioButtons.forEach(element => {
     element.addEventListener('click', function(){
-        myGame = element.value
-        joueur.innerText = element.value
+        if (element.value == 1){
+            myGame = 'pierre'
+            joueur.innerText = myGame
+        }else if(element.value == 2){
+            myGame = 'feuille'
+            joueur.innerText = myGame
+        }else if(element.value == 3){
+            myGame = 'ciseaux'
+            joueur.innerText = myGame
+        }
     })
 })
 
-
-// verify.addEventListener('click', function(){
-//     // let radioButtonsValue = document.querySelectorAll('input[name="btnradio"]:checked').value
-//     // joueur.innerText = radioButtonsValue
-//     for(const radioButton in radioButtons){
-//         if (radioButton.checked){
-//             myGame = radioButton.value
-//             joueur.innerText = radioButton.value
-//         }
-//     }
-// })
+reset.addEventListener('click', function(){
+    let myGame = ''
+    let computerGame = ''
+    joueur.innerText = 'Joueur'
+    ordinateur.innerText = 'Ordinateur'
+})
 
 play.addEventListener('click', function(){
     let randomGame = randomInteger(1,4)
-    // ordinateur.innerText = randomGame
-    // console.log(randomGame)
     if (randomGame == 1){
         computerGame = 'pierre'
-        ordinateur.innerText = randomGame
+        ordinateur.innerText = computerGame
     }else if(randomGame == 2){
         computerGame = 'feuille'
-        ordinateur.innerText = randomGame
+        ordinateur.innerText = computerGame
     }else if(randomGame == 3){
         computerGame = 'ciseaux'
-        ordinateur.innerText = randomGame
+        ordinateur.innerText = computerGame
     }
 
 })
